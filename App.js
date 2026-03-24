ced17e5c74b55c35624a76694b2032e4fa2d1609
shadow-Sx <shadowsoya8@gmail.com>
2026-03-24 04:44:49 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Image, FlatList } from 'react-native';
+
+// Asosiy ilova
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>AniManxwa - Manga Reader</Text>
+      <Text>Ilova ishga tushdi! ✅</Text>
+    </View>
+  );
+}
