d283aa0886a97f51984f828ef02e26404a9012c4
shadow-Sx <shadowsoya8@gmail.com>
2026-03-24 04:51:56 +0300

    Update mega_backend.py

--- server/mega_backend.py
+++ server/mega_backend.py
@@ -3,15 +3,12 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
-# Mega.nz ga ulanish
 mega = Mega()
 m = mega.login('email@mega.nz', 'parolingiz')
 
-@app.route('/rasm/<manga_id>/<bob>//<sahifa>')
-def rasm_olish(manga_id, bob, sahifa):
-    # Mega.nz dan rasmni topib yuborish
-    rasm_yoli = f'Mangas/{manga_id}/chapter_{bob}/{sahifa}.jpg'
-    fayl = m.find(rasm_yoli)
+@app.route('/<manga>/<bob>/<sahifa>')
+def manga(manga, bob, sahifa):
+    fayl = m.find(f'Mangas/{manga}/chapter_{bob}/{sahifa}.jpg')
     return send_file(m.download(fayl), mimetype='image/jpeg')
 
 if __name__ == '__main__':
