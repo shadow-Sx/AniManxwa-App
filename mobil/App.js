5dd50d378e00f332c9c48925662fe44cde6c471f
shadow-Sx <shadowsoya8@gmail.com>
2026-03-24 04:51:12 +0300

    Update and rename App.js to mobil/App.js

---
+++ mobil/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>AniManxwa</Text>
+      <Text>Manga Reader ilovasi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
